d57c415618590dc79bd235b0f05c67b4b48c1868
qiokio <qiokio@126.com>
2025-06-01 11:49:25 +0800

    fix

--- .wrangler/tmp/bundle-TXDLoj/middleware-loader.entry.ts
+++ .wrangler/tmp/bundle-TXDLoj/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "d:\\Project\\sso\\worker\\.wrangler\\tmp\\bundle-6kWoPr\\middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "d:\\Project\\sso\\worker\\.wrangler\\tmp\\bundle-TXDLoj\\middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "D:\\Program Files\\nodejs\\node_cache\\node_modules\\wrangler\\templates\\middleware\\common.ts";
-import type { WorkerEntrypointConstructor } from "d:\\Project\\sso\\worker\\.wrangler\\tmp\\bundle-6kWoPr\\middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "d:\\Project\\sso\\worker\\.wrangler\\tmp\\bundle-TXDLoj\\middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "d:\\Project\\sso\\worker\\.wrangler\\tmp\\bundle-6kWoPr\\middleware-insertion-facade.js";
+export * from "d:\\Project\\sso\\worker\\.wrangler\\tmp\\bundle-TXDLoj\\middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
--- .wrangler/tmp/dev-0AtC5x/index.js
+++ .wrangler/tmp/dev-0AtC5x/index.js
@@ -33,14 +33,14 @@ var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__ge
 ));
 var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 
-// .wrangler/tmp/bundle-6kWoPr/strip-cf-connecting-ip-header.js
+// .wrangler/tmp/bundle-TXDLoj/strip-cf-connecting-ip-header.js
 function stripCfConnectingIPHeader(input, init) {
   const request = new Request(input, init);
   request.headers.delete("CF-Connecting-IP");
   return request;
 }
 var init_strip_cf_connecting_ip_header = __esm({
-  ".wrangler/tmp/bundle-6kWoPr/strip-cf-connecting-ip-header.js"() {
+  ".wrangler/tmp/bundle-TXDLoj/strip-cf-connecting-ip-header.js"() {
     "use strict";
     __name(stripCfConnectingIPHeader, "stripCfConnectingIPHeader");
     globalThis.fetch = new Proxy(globalThis.fetch, {
@@ -53179,14 +53179,14 @@ var require_build = __commonJS({
   }
 });
 
-// .wrangler/tmp/bundle-6kWoPr/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-TXDLoj/middleware-loader.entry.ts
 init_strip_cf_connecting_ip_header();
 init_modules_watch_stub();
 init_virtual_unenv_global_polyfill_cloudflare_unenv_preset_node_process();
 init_virtual_unenv_global_polyfill_cloudflare_unenv_preset_node_console();
 init_performance2();
 
-// .wrangler/tmp/bundle-6kWoPr/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-TXDLoj/middleware-insertion-facade.js
 init_strip_cf_connecting_ip_header();
 init_modules_watch_stub();
 init_virtual_unenv_global_polyfill_cloudflare_unenv_preset_node_process();
@@ -57103,7 +57103,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env2, _ctx, middlewareCtx
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-6kWoPr/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-TXDLoj/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -57140,7 +57140,7 @@ function __facade_invoke__(request, env2, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-6kWoPr/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-TXDLoj/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
